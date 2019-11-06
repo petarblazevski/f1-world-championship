86934f1d79adc0f967d00006d90e5eb9c8de80b8
Petar Blazevski <petar.blazevski@gmail.com>
2019-11-06 19:08:41 +0100

    display the years available years

--- src/app/containers/home/home.component.spec.ts
+++ src/app/containers/home/home.component.spec.ts
@@ -8,9 +8,8 @@ describe('HomeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
-    })
-    .compileComponents();
+      declarations: [HomeComponent]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -21,5 +20,18 @@ describe('HomeComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should contain an array of years to display', () => {
+    expect(component.years.length).toEqual(component.maxYear - component.minYear + 1);
+  });
+
+  it('should should render a list of available years', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('.championship-years')).not.toBeNull();
+    expect(compiled.querySelectorAll('.championship-years__year').length).toEqual(
+      component.maxYear - component.minYear + 1
+    );
+    expect(compiled.querySelectorAll('.championship-years__year')[0].innerHTML).toEqual(component.minYear.toString());
   });
 });
--- src/app/containers/home/home.component.ts
+++ src/app/containers/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  minYear = 2005;
+  maxYear = 2015;
 
-  constructor() { }
+  years: number[];
 
-  ngOnInit() {
+  constructor() {
+    this.years = new Array<number>(this.maxYear - this.minYear + 1).fill(undefined).map((x, i) => this.minYear + i);
   }
 
+  ngOnInit() {}
 }
