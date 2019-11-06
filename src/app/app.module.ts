de215feb74f12e5103157f1d4c20e97c642872d7
Petar Blazevski <petar.blazevski@gmail.com>
2019-11-06 20:25:24 +0100

    import ngrx into the app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { HttpClientModule } from '@angular/common/http';
 
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
 import { AppRoutingModule } from './app-routing.module';
 import * as fromContainers from './containers';
 
+import { reducers, metaReducers } from './store';
+import { environment } from '../environments/environment';
+import { ChampionshipEffects } from './store/championship.effects';
+
 @NgModule({
   declarations: [...fromContainers.containers],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    StoreModule.forRoot(reducers, {
+      metaReducers,
+      runtimeChecks: {
+        strictStateImmutability: true,
+        strictActionImmutability: true
+      }
+    }),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    EffectsModule.forRoot([ChampionshipEffects]),
+    StoreRouterConnectingModule.forRoot()
+  ],
   providers: [],
   bootstrap: [fromContainers.AppComponent]
 })
