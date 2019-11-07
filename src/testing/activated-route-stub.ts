d9e931342d101f7e2392b696ae834a57b3ffa4f6
Petar Blazevski <petar.blazevski@gmail.com>
2019-11-07 19:03:36 +0100

    add application stubs

---
+++ src/testing/activated-route-stub.ts
@@ -0,0 +1,35 @@
+import { convertToParamMap, ParamMap, Params } from '@angular/router';
+import { ReplaySubject } from 'rxjs';
+
+/**
+ * An ActivateRoute test double with a `paramMap` observable.
+ * Use the `setParamMap()` method to add the next `paramMap` value.
+ */
+export class ActivatedRouteStub {
+  // Use a ReplaySubject to share previous values with subscribers
+  // and pump new values into the `paramMap` observable
+  private subject = new ReplaySubject<ParamMap>();
+
+  snapshot;
+
+  constructor(initialParams?: Params) {
+    this.setParamMap(initialParams);
+
+    this.setSnapshotParamMap(initialParams);
+  }
+
+  /** The mock paramMap observable */
+  readonly paramMap = this.subject.asObservable();
+
+  /** Set the paramMap observables's next value */
+  setParamMap(params?: Params) {
+    this.subject.next(convertToParamMap(params));
+  }
+
+  /** Set the snapshot paramMap */
+  setSnapshotParamMap(params?: Params) {
+    this.snapshot = {
+      paramMap: convertToParamMap(params)
+    };
+  }
+}
