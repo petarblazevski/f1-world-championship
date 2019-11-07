7156a362f3bbb12e8f806229bbb1dece58750bea
Petar Blazevski <petar.blazevski@gmail.com>
2019-11-07 19:04:20 +0100

    add results page

--- src/app/containers/results/results.component.spec.ts
+++ src/app/containers/results/results.component.spec.ts
@@ -1,25 +1,74 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ActivatedRoute } from '@angular/router';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { Store } from '@ngrx/store';
 
 import { ResultsComponent } from './results.component';
+import { IsWorldChampionPipe } from '../../pipes/is-world-champion.pipe';
+import * as fromStore from '../../store';
+import * as championshipSelectors from '../../store/championship.selectors';
+import * as championshipActions from '../../store/championship.actions';
+import { ActivatedRouteStub } from '../../../testing/activated-route-stub';
 
 describe('ResultsComponent', () => {
   let component: ResultsComponent;
   let fixture: ComponentFixture<ResultsComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ResultsComponent ]
-    })
-    .compileComponents();
-  }));
+  let store: MockStore<{ championship: { loading: boolean } }>;
+  const initialState = { championship: { loading: false } };
+  let activatedRoute: ActivatedRouteStub;
 
   beforeEach(() => {
+    activatedRoute = new ActivatedRouteStub();
+  });
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      declarations: [ResultsComponent, IsWorldChampionPipe],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: activatedRoute
+        },
+        provideMockStore({
+          initialState,
+          selectors: [{ selector: championshipSelectors.selectChampionshipLoading, value: false }]
+        })
+      ]
+    }).compileComponents();
+  }));
+
+  beforeEach(async () => {
+    activatedRoute.setSnapshotParamMap({ year: 2008 });
     fixture = TestBed.createComponent(ResultsComponent);
+    store = TestBed.get<Store<fromStore.State>>(Store);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    spyOn(store, 'dispatch');
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should dispatch championshipActions.load action', () => {
+    const action = championshipActions.load({ year: 2008 });
+
+    fixture.detectChanges();
+    component.ngOnInit();
+
+    expect(store.dispatch).toHaveBeenCalledWith(action);
+  });
+
+  it('should dispatch championshipActions.clear action when the component is destroyed', () => {
+    const action = championshipActions.clear();
+
+    fixture.detectChanges();
+    component.ngOnDestroy();
+
+    expect(store.dispatch).toHaveBeenCalledWith(action);
   });
 });
--- src/app/containers/results/results.component.ts
+++ src/app/containers/results/results.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { select, Store } from '@ngrx/store';
+import { Observable, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
+import { State } from '../../store';
+
+import { IRace } from '../../models/race';
+import { IDriverStanding } from '../../models/driver-standing';
+import * as championshipActions from '../../store/championship.actions';
+import * as championshipSelectors from '../../store/championship.selectors';
 
 @Component({
   selector: 'app-results',
   templateUrl: './results.component.html',
   styleUrls: ['./results.component.scss']
 })
-export class ResultsComponent implements OnInit {
+export class ResultsComponent implements OnInit, OnDestroy {
+  private unsubscribe$: Subject<any> = new Subject<any>();
 
-  constructor() { }
+  loading$: Observable<boolean> = this.store.pipe(
+    select(championshipSelectors.selectChampionshipLoading),
+    takeUntil(this.unsubscribe$)
+  );
+
+  races$: Observable<IRace[]> = this.store.pipe(
+    select(championshipSelectors.selectChampionshipRaces),
+    takeUntil(this.unsubscribe$)
+  );
+
+  champion$: Observable<IDriverStanding> = this.store.pipe(
+    select(championshipSelectors.selectChampionshipChampion),
+    takeUntil(this.unsubscribe$)
+  );
+
+  constructor(private store: Store<State>, private route: ActivatedRoute) {}
 
   ngOnInit() {
+    const year = parseInt(this.route.snapshot.paramMap.get('year'), 10);
+    this.store.dispatch(championshipActions.load({ year }));
   }
 
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+
+    this.store.dispatch(championshipActions.clear());
+  }
 }
