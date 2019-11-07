af95db369921d210b1204b470a72f82aa2ab9f6a
Petar Blazevski <petar.blazevski@gmail.com>
2019-11-07 22:22:27 +0100

    add loading spinner

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,7 +1,9 @@
 import { HeaderComponent } from './header/header.component';
 import { ResultsListComponent } from './results-list/results-list.component';
+import { LoadingComponent } from './loading/loading.component';
 
-export const components: any[] = [HeaderComponent, ResultsListComponent];
+export const components: any[] = [HeaderComponent, ResultsListComponent, LoadingComponent];
 
 export * from './header/header.component';
 export * from './results-list/results-list.component';
+export * from './loading/loading.component';
